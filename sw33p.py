43159fefafb32dbdd7b4ba17b4d8554b105771e3
Robert <robert@pcextreme.nl>
2016-10-10 21:45:30 +0200

    added comments and replaced tabs with spaces

--- sw33p.py
+++ sw33p.py
@@ -5,38 +5,40 @@ import sys
 import socket
 import subprocess
 
+# Check for root
 if os.geteuid() != 0:
-	sys.exit('Run as root!')
+    sys.exit('This program needs root privileges!')
 
+# Check if interface has been given
 try:
-	interface = sys.argv[1]
-	interfaces = os.listdir('/sys/class/net')
-	if not interface in interfaces:
-		sys.exit(0)
+    interface = sys.argv[1]
+    interfaces = os.listdir('/sys/class/net')
+    if not interface in interfaces:
+        sys.exit()
 except:
-	print('No valid interface.')
-	sys.exit(0)
+    print('No valid interface was given.')
+    sys.exit()
 
+# Socket setup
 sweeper = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
-sweeper.bind((interface, 0x0003))
+sweeper.bind((interface, 0x003)) # Bind socket to interface
 
-def check_header_lenght(pkt):
-	header = str(pkt[2:4].hex()).rstrip('0')
-	l = 8 * len(header)
-	lenght = int(header, l)
-	return lenght 
+# Header length value is stored on the 5:9 byte or 2:4 hex
+def check_header_length(packet):
+    header = str(packet[2:4].hex()).rstrip('0')
+    byte = 8 * len(header)
+    length = int(header, byte)
+    return length
 
+# sw33p
+i = 0
 while True:
-	for channel in range(1, 13):
-		subprocess.run(['iwconfig', interface, 'channel', str(channel)])
-		print('Channel: {}'.format(channel))
-		packet = sweeper.recvfrom(2048)[0]
-
-		if packet[check_header_lenght(packet)] == 128:
-			i = i + 1
-			print('{} :This is an frame and has a header_leght of {}'.format(i, check_header_lenght(packet)))
-
-
-check_header_lenght(packet)
-
-
+    # Change channels and scan
+    for channel in range(1, 14):
+        subprocess.run(['iwconfig', interface, 'channel', str(channel)])
+        print('Scanning channel {} ...'.format(channel))
+        packet = sweeper.recvfrom(2048)[0]
+    # Check if packet is an beacon_frame
+        if packet[check_header_length(packet)] == 128:
+            i = i + 1
+            print('{}: Found beacon_frame with header_length of {} \n'.format(i, check_header_length(packet)))
