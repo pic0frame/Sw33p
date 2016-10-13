f5e31b3c57ee04fe6a7c6a03ea28d2eb19e2f67f
Robert <robert@nanohost.nl>
2016-10-13 20:35:06 +0200

    tijdelijke commit

--- sw33p.py
+++ sw33p.py
@@ -29,44 +29,72 @@ except:
 sweeper = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
 sweeper.bind((interface, 0x003)) # Bind socket to interface
 
-# Decode length byte
+# Decode length byte into decimals
 def decode_length( pkt, start, end):
-    #print(packet)
     item = pkt[start:end].hex()
     if not item == '10':
         item = item.rstrip('0')
-    item_l = len(item)
     item = int(item, 8 * len(item))
     return item
 
-# Change channels and scan
+# def decode_info(pkt):
+#     radiotap = decode_length(pkt, 2, 4)
+#     beacon = radiotap + 24
+#     fixed_parm = beacon + 12
+#     essid_l = decode_length(pkt, int(fixed_parm+2), int(fixed_parm+3))
+#     ESSID = fixed_parm+2 + essid_l
+#     essid = packetx2[fixed_parm+2:fixed_parm+2 + essid_l].decode()
+#     supported_rates = (ESSID + 1) + decode_length(pkt, ESSID+1, ESSID+2)
+#     CH = decode_length(pkt, supported_rates+2, supported_rates+3)
+#     MAC = str(':'.join(re.findall('.{1,2}', pkt[radiotap+10:radiotap+17].hex()))).upper
+
+# Change channels
 def channel_swap():
     while True:
         for channel in range(1, 14):
             subprocess.run(['iwconfig', interface, 'channel', str(channel)])
-           # time.sleep(float(0.4))
+            #time.sleep(float(0.5))
             #channel = channel
             #return 1
 
 def rest():
-    # Check if packet is an beacon_frame
     i = 0
+    #time.sleep()
     while True:
+        # sw33p packet
         packet = sweeper.recvfrom(2048)[0]
-        if packet[decode_length(packet, 2, 4)] == 128 or packet[2:4] != b'&\x00':
+        # Check if packet is an beacon_frame
+        if packet[decode_length(packet, 2, 4)] == 128:
+            # Try to decode ESSID (some packets look like beacons but they are not)
             try:
-                SSID = packet[76:(76 + decode_length(packet, 75, 76))].decode()
+               # decode_info(packet)
+                radiotap = decode_length(packet, 2, 4)
+                beacon = radiotap + 24
+                fixed_parm = beacon + 12
+                essid_l = decode_length(packet, int(fixed_parm+2), int(fixed_parm+3))
+                ESSID = fixed_parm+2 + essid_l
+                essid = packet[fixed_parm+2:fixed_parm+2 + essid_l].decode()
+                print(essid)
+                supported_rates = (ESSID + 1) + decode_length(packet, ESSID+1, ESSID+2)
+                CH = decode_length(packet, supported_rates+2, supported_rates+3)
+                print(CH)
+                MAC = ':'.join(re.findall('.{1,2}', packet[radiotap+10:radiotap+17].hex()))
+                print(MAC)
+
             except:
-                print('Error: Deoding SSID')
-        #   print(packet[76])
-            MAC = ':'.join(re.findall('.{1,2}', packet[48:55].hex()))
-            #list_apx1 = [str(i), SSID, MAC, channel]    
-                #print(packet)
-            i = i+1
-            print('Count: {} | {}'.format(i, str(SSID)))
-            print('\t MAC: {}'.format(str(MAC).upper()))
-            #time.sleep(float(0.5))
-            #print('\t CH : {}\n'.format(decode_length(packet, 112, 113)))
+                pass
+                print('decode_info failed!!')
+
+            #print(radiotap)
+            #print(beacon)
+            #print(fixed_parm)
+            #print(essid_l)
+            #print(ESSID)
+            #print(essid)
+            #print(MAC)
+            #print(supported_rates)
+            #print('{}\n'.format(CH))
+
 
 # Thread setup
 import threading
